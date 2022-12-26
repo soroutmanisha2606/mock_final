e5073394e0db30c043382d9cda067d8f7be50acb
manisha <soroutmanisha12@gmail>
2022-12-27 01:38:37 +0530

    update

--- db/connect.js
+++ db/connect.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose');
 // const { reset } = require('nodemon');
 async function connect(){
     return new Promise((resolve,reject)=>{
-        mongoose.connect('mongodb://127.0.0.1:27017',(err)=>{
+        mongoose.connect('mongodb+srv://manisha123:manisha123@cluster0.tfr1v5n.mongodb.net/?retryWrites=true&w=majority',(err)=>{
             if(err){
                 console.log('error in connection with database')
                 reject(err)
